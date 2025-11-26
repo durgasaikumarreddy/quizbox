f0df91561dc83ca5d89a4b0ce2f7e99814aadde2
durgasaikumarreddy <durgasaikumarreddy@gmail.com>
2025-11-26 15:28:15 +0000

    feat: Add Create Quiz page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AdminLogin from "./pages/AdminLogin";
+import CreateQuiz from "./pages/CreateQuiz";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/admin/login" element={<AdminLogin />} />
+        <Route path="/quizzes/create" element={<CreateQuiz />} />
       </Routes>
     </BrowserRouter>
   );
