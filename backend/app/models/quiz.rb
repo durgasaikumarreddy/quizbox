7388ad3499f20b885b79f5b3914c6201402945cf
durgasaikumarreddy <durgasaikumarreddy@gmail.com>
2025-11-26 12:39:01 +0000

    feat: Update quiz model to validate question params

--- backend/app/models/quiz.rb
+++ backend/app/models/quiz.rb
@@ -1,5 +1,28 @@
 class Quiz < ApplicationRecord
-  has_many :questions
+  has_many :questions, dependent: :destroy
+  accepts_nested_attributes_for :questions
 
   validates :title, presence: true
+  validates :must_have_at_least_one_question
+  validates :all_questions_must_be_valid
+
+  private
+
+  # Custom validation to ensure at least one question is associated with the quiz
+  def must_have_at_least_one_question
+    if questions.empty?
+      errors.add(:quiz, "must have at least one question")
+    end
+  end
+
+  # Custom validation to ensure all associated questions are valid
+  def all_questions_must_be_valid
+    questions.each_with_index do |question, index|
+      next if question.valid?
+
+      question.errors.full_messages.each do |msg|
+        errors.add(:quiz, "Question #{index + 1}: #{msg}")
+      end
+    end
+  end
 end
