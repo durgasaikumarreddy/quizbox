2711080463f854c9ed888437ac40782bd556e625
durgasaikumarreddy <durgasaikumarreddy@gmail.com>
2025-11-26 14:10:19 +0000

    feat: Add route for submit

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   post "/admin/login", to: "admin#login"
 
-  resources :quizzes, only: [:create, :index, :show]
+  resources :quizzes, only: [:create, :index, :show] do
+    post :submit, on: :member
+  end
 end
